e8cafcbd1fc10f268416169343d626d9d7303f36
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-07 22:09:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(list, name) {
   return list.filter(function (driverName) {
-    return driverName.tolowerCase();
+    return driverName.toLowerCase();
   })
 }
 
