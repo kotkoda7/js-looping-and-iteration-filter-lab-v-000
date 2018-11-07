91273132fd72ceff96e18b0ba80c8e0fc2141ddb
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-07 22:08:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(list, name) {
   return list.filter(function (driverName) {
-    return driverName.tolowerCase()
+    return driverName.tolowerCase();
   })
 }
